c5c57e337f7c038c2e8f1df2a761aa2894a7f7ce
jasenon <1921157395@qq.com>
2026-04-14 12:50:54 +0800

    init(项目模块拆分): lab-system-cloud版本  准备微服务化

---
+++ quartz/src/test/java/xyz/jasenon/lab/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jasenon.lab.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
